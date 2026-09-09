68a44ba5bd8fd152ea9454d19bdd8e0eabb631ee
Jaromil <jaromil@dyne.org>
2026-09-09 10:31:45 +0200

    ci: add change-gated nightly assurance

---
+++ ci/test/00_setup_env_native_extended_functional.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026-present The Bitcoin Roots developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_native_extended_functional
+export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
+export PACKAGES="python3-zmq libevent-dev libboost-dev libsqlite3-dev libdb++-dev libminiupnpc-dev"
+export NO_DEPENDS=1
+export RUN_UNIT_TESTS=false
+export RUN_FUNCTIONAL_TESTS=true
+export RUN_FUZZ_TESTS=false
+export TEST_RUNNER_EXTRA="--extended"
+export GOAL="install"
+export BITCOIN_CONFIG="-DWITH_ZMQ=ON -DWITH_BDB=ON -DWARN_INCOMPATIBLE_BDB=OFF -DWITH_MINIUPNPC=ON"
