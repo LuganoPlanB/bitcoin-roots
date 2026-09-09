dee4c017b0c59f883df3032a8d66561cc04573d2
Jaromil <jaromil@dyne.org>
2026-09-09 10:41:24 +0200

    ci: align platform and cache boundaries

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -7,11 +7,13 @@
 export LC_ALL=C.UTF-8
 
 export HOST=i686-pc-linux-gnu
-export CONTAINER_NAME=ci_i686_multiprocess
+export CONTAINER_NAME=ci_i686_debug
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
 export CI_IMAGE_PLATFORM="linux/amd64"
 export PACKAGES="llvm clang g++-multilib"
-export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
+# This legacy filename is retained for CI references. Multiprocess is disabled
+# at the top level by CMake, so this is an i686 Debug configuration only.
+export DEP_OPTS="DEBUG=1"
 export GOAL="install"
 export TEST_RUNNER_EXTRA="--v2transport"
 export BITCOIN_CONFIG="\
