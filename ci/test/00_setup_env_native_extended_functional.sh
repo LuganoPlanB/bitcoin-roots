39a867b9ca7619b3ca8d6c49dbe19a58fa1e8298
Jaromil <jaromil@dyne.org>
2026-09-10 14:30:35 +0200

    fix(ci): install ZeroMQ for extended functional tests

--- ci/test/00_setup_env_native_extended_functional.sh
+++ ci/test/00_setup_env_native_extended_functional.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_extended_functional
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
-export PACKAGES="python3-zmq libevent-dev libboost-dev libsqlite3-dev libdb++-dev libminiupnpc-dev"
+export PACKAGES="python3-zmq libzmq3-dev libevent-dev libboost-dev libsqlite3-dev libdb++-dev libminiupnpc-dev"
 export NO_DEPENDS=1
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=true
