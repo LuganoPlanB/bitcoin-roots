9d28327e727553414ea57e80f419f72052de1bfa
Jaromil <jaromil@dyne.org>
2026-09-25 01:19:19 +0200

    fix: restore outbound message capture

--- test/functional/p2p_message_capture.py
+++ test/functional/p2p_message_capture.py
@@ -62,6 +62,8 @@ class MessageCaptureTest(BitcoinTestFramework):
         # Connect a node so that the handshake occurs
         self.nodes[0].add_p2p_connection(P2PDataStore())
         self.nodes[0].disconnect_p2ps()
+        self.wait_until(lambda: glob.glob(os.path.join(capturedir, "*/msgs_recv.dat")))
+        self.wait_until(lambda: glob.glob(os.path.join(capturedir, "*/msgs_sent.dat")))
         recv_file = glob.glob(os.path.join(capturedir, "*/msgs_recv.dat"))[0]
         mini_parser(recv_file)
         sent_file = glob.glob(os.path.join(capturedir, "*/msgs_sent.dat"))[0]
